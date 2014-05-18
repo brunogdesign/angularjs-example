797232f8bf6294fe4329e13de7dd587ca763b39c
Bruno Garcia <oxigenio1@gmail.com>
2014-05-18 19:32:08 +0100

    Update app example to a car store with multiple cars to sell

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,19 +1,30 @@
+
 (function() {
+    
     "use strict";
 
     var app = angular.module('store', []);
 
     app.controller('StoreController', function() {
-
-        this.car = {
+        this.cars = cars;
+    });
+    
+    
+    var cars = [
+        {
             model: 'C4',
             year: 2008,
             brand: 'Citroen',
-            price: 20000
-        };
-
-
-    });
-    
+            price: 20000,
+            canPurchase: true
+        },
+        {
+            model: 'A3',
+            year: 2013,
+            brand: 'Audi',
+            price: 40000,
+            canPurchase: false
+        }
+    ];
     
 })();
